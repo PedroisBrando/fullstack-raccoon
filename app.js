5bc448d2f462447e33a658eec2fc613b8550e1c8
PedroisBrando <pedroibrando@gmail.com>
2020-01-25 15:33:19 -0300

    Item 'a' completed

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
-const axios = require('axios');
+const axios = require("axios");
 
-function filterByMedia(obj){
-  if(obj.title.includes('promocao')) return true
+function filterByPromocao(obj) {
+  if (obj.title.includes("promocao")) return true;
 }
 
-function getPosts() {
-  return axios.get('https://us-central1-psel-clt-ti-junho-2019.cloudfunctions.net/psel_2019_get')  
+function getPostsPromocao() {
+  return axios
+    .get(
+      "https://us-central1-psel-clt-ti-junho-2019.cloudfunctions.net/psel_2019_get"
+    )
     .then(response => {
-      var filtered = response.data.posts.filter(filterByMedia);
+      var filtered = response.data.posts.filter(filterByPromocao);
       return filtered;
-    })
+    });
 }
 
-function teste(){
-  const found = posts.find(element => {
-    element.media === 'instagram_cpc'
-  })
+function compare(a, b) {
+  if (a.price < b.price) {
+    return -1;
+  }
+  if (a.price > b.price) {
+    return 1;
+  }
+  if (a.price === b.price) {
+    if (a.product_id < b.product_id) {
+      return -1;
+    }
+    if (a.product_id > b.product_id) {
+      return 1;
+    }
+    return 0;
+  }
+  return 0;
 }
 
-getPosts().then(data => {
-  console.log(data);
-}); 
+getPostsPromocao().then(res => {
+  res.sort(compare);
+  let teste = res[0].product_id;
+  res.map(data => {
+    if (data.product_id !== teste) console.log(data.product_id, data.price);
+    teste = data.product_id;
+  });
+});
