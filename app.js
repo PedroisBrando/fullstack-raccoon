06293e9c79e864adde68a32d9fcf88c0d56a0db4
PedroisBrando <pedroibrando@gmail.com>
2020-01-26 13:04:52 -0300

    Item b, c Done. Doing item d

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ function filterByPromocao(obj) {
   if (obj.title.includes("promocao")) return true;
 }
 
+function filterByInstagram(obj) {
+  if (obj.media.includes("instagram_cpc") && obj.likes > 700) return true;
+}
+
+function filterByMediaAndMonth(obj) {
+  const medias = ['google_cpc', 'facebook_cpc', 'instagram_cpc'];
+  if (medias.some(el => obj.media.includes(el)) && obj.date.includes('/05/')) return true;
+}
+
 function getPostsPromocao() {
   return axios
     .get(
@@ -13,6 +22,37 @@ function getPostsPromocao() {
       var filtered = response.data.posts.filter(filterByPromocao);
       return filtered;
     });
+}
+
+function getPosts700() {
+  return axios
+    .get(
+      "https://us-central1-psel-clt-ti-junho-2019.cloudfunctions.net/psel_2019_get"
+    )
+    .then(response => {
+      var filtered = response.data.posts.filter(filterByInstagram);
+      return filtered;
+    })
+}
+
+function getPostsSomatorio() {
+  return axios
+    .get(
+      "https://us-central1-psel-clt-ti-junho-2019.cloudfunctions.net/psel_2019_get"
+    )
+    .then(response => {
+      var filtered = response.data.posts.filter(filterByMediaAndMonth);
+      return filtered;
+    })
+}
+
+function getPostsError() {
+  return axios
+    .get(
+      "https://us-central1-psel-clt-ti-junho-2019.cloudfunctions.net/psel_2019_get_error"
+    ).then(response => {
+      return response.data.posts;
+    })
 }
 
 function compare(a, b) {
@@ -34,7 +74,17 @@ function compare(a, b) {
   return 0;
 }
 
-getPostsPromocao().then(res => {
+function compareId(a, b) {
+  if (a.product_id < b.product) {
+    return -1;
+  }
+  if (a.product > b.product) {
+    return 1;
+  }
+  return 0;
+}
+
+/*getPostsPromocao().then(res => {
   res.sort(compare);
   let teste = res[0].product_id;
   res.map(data => {
@@ -42,3 +92,33 @@ getPostsPromocao().then(res => {
     teste = data.product_id;
   });
 });
+*/
+
+/*getPosts700().then(res => {
+  res.sort(compare);
+  res.map(data => {
+    console.log(data.product_id, data.price);
+  })
+})
+*/
+
+/*getPostsSomatorio().then(res => {
+  let somatorio = 0;
+  res.map(data => {
+    console.log(data.media, data.date, data.likes);
+    somatorio += data.likes;
+  })
+  console.log(somatorio);
+})
+*/
+
+getPostsError().then(res => {
+  res.sort(compare);
+  let teste = res[0].product_id;
+  let erros = [];
+  res.map(data => {
+    console.log(data.product_id, data.price);
+    //if(data.product_id === teste && data.price !== teste.price) erros.push(data);
+  })
+  console.log(erros);
+})
